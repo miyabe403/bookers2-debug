39c08652fed6d3b2172d805eb1d3a28877a5c69c
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-22 17:48:12 +0900

    [Fix] 新規投稿フォームに何も情報が入ってない空のモデルを指定

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,9 +1,9 @@
 class Book < ApplicationRecord
   # Book モデルには、ActiveStorage を使って画像を持たせます。
-  has_one_attached :profile_image 
+  has_one_attached :profile_image
    
   # has_many :user
-  belongs_to :user 
+  belongs_to :user
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
 end
