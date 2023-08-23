7f6e9c2a11d0e5d1cecfa7b2afe243a32f3f385b
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-23 19:53:47 +0900

    [Add] BookモデルとBookCommentモデルを関連付ける（Bookモデルへの記述）

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,7 +4,7 @@ class Book < ApplicationRecord
    
   # has_many :user
   belongs_to :user
-  has_many :book_comments, dependent: :destroy
+  has_many :book_comments, dependent: :destroy 
   has_many :favorites, dependent: :destroy  # Bookモデルにも追加
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
