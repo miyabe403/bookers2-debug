788d95d12bd55471801191fb0e6b48f6066057ed
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-23 14:04:14 +0900

    [Add] Favoriteモデルのカラム（user_idとbook_id）を、UserモデルのidやBookモデルのidに関連付けを設定

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,3 +1,6 @@
 class Favorite < ApplicationRecord
-
+  
+  belongs_to :user  # Favoriteモデルのカラム（user_idとbook_id）を、UserモデルのidやBookモデルのidに関連付けを設定
+  belongs_to :book  # Favoriteモデルのカラム（user_idとbook_id）を、UserモデルのidやBookモデルのidに関連付けを設定
+  
 end
