8a6b9adadbf5c6e604b7f66bfc722b9a622d2421
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-24 16:45:02 +0900

    [Add] relationshipモデルにリレーションを記述

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,3 +1,4 @@
 class Relationship < ApplicationRecord
-  
+  belongs_to :follower, class_name: "User" # class_nameでクラス名（モデル名）を指定可能 これにより、正しくusersテーブルを参照
+  belongs_to :followed, class_name: "User" # 関連名と参照先のクラス名を異なるものに置き換えることができるオプション。
 end
