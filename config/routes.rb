20d4be6810df670a2ad21185e02787a6671a19c3
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-24 19:44:39 +0900

    [Add] ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,12 @@ Rails.application.routes.draw do
     resources :book_comments, only: [:create, :destroy] 
     resource :favorites, only: [:create, :destroy]
   end
-  resources :users, only: [:index,:show,:edit,:update]
+  resources :users, only: [:index,:show,:edit,:update] do
+    member do # memberとはresourcesで生成されるルートに、決められたルート以外のルートを追加するための処理
+      get :follows, :followers
+    end # Userと、Relationshipは関連づけられているためuserのidが必要なのでrelationshipsをネストする。
+      resource :relationships, only: [:create, :destroy]
+  end
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end # endを追加
