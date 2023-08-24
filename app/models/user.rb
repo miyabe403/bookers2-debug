0a29bd77a8ef06a0bc015aa0cdafa635df5bac75
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-24 17:45:44 +0900

    [Add] userモデルにリレーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,20 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy 
   has_many :book_comments, dependent: :destroy 
   has_many :favorites, dependent: :destroy  # Userモデルにも追加
+  
+  # フォローをした、されたの関係
+  # has_many :xxxはアソシエーションが繋がっているテーブル名、class_nameは実際のモデルの名前、foreign_keyは外部キーとして何を持つかを表しています。
+  has_many :followers, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followeds, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+  # 一覧画面で使う
+  #「has_many :テーブル名, through: :中間テーブル名」 の形を使って、テーブル同士が中間テーブルを通じてつながっていることを表現します。(followerテーブルとfollowedテーブルのつながりを表す）
+  # 例えば、has_many :yyyにfollowedを入れてしまうと、followedテーブルから中間テーブルを通ってfollowerテーブルにアクセスすることができなくなってしまいます。
+  #  これを防ぐためにhas_many :yyyには架空のテーブル名を、source: :zzzは実際にデータを取得しにいくテーブル名を書きます。
+  has_many :following_users, through: :followers, source: :followed
+  has_many :follower_users, through: :followeds, source: :follower
+  
+  #この結果、@user.yyyとすることでそのユーザーがフォローしている人orフォローされている人の一覧を表示することができるようになります。
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: {maximum: 50}  # introductionカラムの追加 
