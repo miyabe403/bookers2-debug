7b8fc30b16a37c6f9370d13b909921449a5e7b95
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-26 12:12:05 +0900

    [Add] いいね保存、削除のリダイレクト先を削除

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,13 +4,13 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
-    redirect_to book_path(book)
+    #redirect_to book_path(book) #いいね保存のリダイレクト先を削除
   end
 
   def destroy
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
-    redirect_to book_path(book)
+    #redirect_to book_path(book) #いいね削除のリダイレクト先を削除
   end
 end
