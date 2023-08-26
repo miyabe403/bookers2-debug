4f4725d02b41a7b154e2f13e027abe1423a996c7
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-26 12:56:56 +0900

    [Add] js.erbファイルの編集

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,13 +4,13 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
-    #redirect_to book_path(book) #いいね保存のリダイレクト先を削除
+    #redirect_to book_path(book) #いいね保存のリダイレクト先を削除 createアクション実行後は、create.js.erbファイルを、探すようになります。
   end
 
   def destroy
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
-    #redirect_to book_path(book) #いいね削除のリダイレクト先を削除
+    #redirect_to book_path(book) #いいね削除のリダイレクト先を削除 destroyアクション実行後はdestroy.js.erbファイルを探すようになります。
   end
 end
