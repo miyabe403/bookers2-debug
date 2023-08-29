a9651d519a0548a02ba902616d31146edb9fcbb9
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-29 11:04:06 +0900

    [Fix] 課題7a/8a/9a【SNSの機能を学ぼう】

--- app/models/user_room.rb
+++ app/models/user_room.rb
@@ -2,5 +2,5 @@ class UserRoom < ApplicationRecord
   belongs_to :user
   belongs_to :room
   
-  validates :message, length: { in: 1..140 } # 1~140文字内でないといけないというバリデーション
+  # validates :message, length: { in: 1..140 } # 1~140文字内でないといけないというバリデーション
 end
