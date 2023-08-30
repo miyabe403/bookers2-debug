7675a4f879f3e6bc919edfa1ad2a5345fb52908a
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-30 22:28:15 +0900

    [Add] ソースコードで .envに記述した環境変数を使用

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    user_name:            ENV['WELCOME_MAILER_ADDRESS'], 
+    user_name:            ENV['WELCOME_MAILER_ADDRESS'],  
     password:             ENV['WELCOME_MAILER_PASSWORD'],
     authentication:       'login',
     enable_starttls_auto: true
