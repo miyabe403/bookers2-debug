0a96cc8f5e2f109904accaf112167d872e589912
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-30 22:03:52 +0900

    [Add] モデルの記述

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ApplicationRecord
   has_many :group_users, dependent: :destroy
   belongs_to :owner, class_name: 'User'
-  has_many :users, through: :group_users, source: :user 
+  has_many :users, through: :group_users, source: :user
   
   validates :name, presence: true
   validates :introduction, presence: true
